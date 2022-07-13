c2d3f06ee4cc8ad81a373f6068af2b6433814dbb
sabzuski <sabzuski@gmail.com>
2022-07-13 13:59:22 -0400

    made changes to functions to identify the fighter first then check for title fight

--- Scripts/fighterStats.py
+++ Scripts/fighterStats.py
@@ -2,9 +2,11 @@ import pandas as pd
 import plotly.express as px
 
 
-def fighterStats(fighter):
+def fighterStats(fighter, title=False):
     df = pd.read_csv("C:\\Users\\sabzu\\Documents\\UFCRecommendationProject\\UFCProject\\DataFiles2\\CleanData.csv", index_col=0)
-
+    df = df[df["BOUT"].str.contains(fighter)]
+    if title and ('Yes' in list(df["TitleFight"])) | ('Interim' in list(df["TitleFight"])):
+        df = df[(df['TitleFight'] == 'Yes') | (df['TitleFight'] == 'Interim')]
     redDF = df.filter(regex='RED$', axis=1)
     redDF["Fight_Time_(Min)"] = df["Fight_Time_(sec)"] / 60
     blueDF = df.filter(regex='BLUE$', axis=1)
@@ -18,9 +20,12 @@ def fighterStats(fighter):
 
     return pd.concat([redDF, blueDF]).sort_index()
 
-def opponentStats(fighter):
+
+def opponentStats(fighter, title=False):
     df = pd.read_csv("C:\\Users\\sabzu\\Documents\\UFCRecommendationProject\\UFCProject\\DataFiles2\\CleanData.csv", index_col=0)
     df = df[df["BOUT"].str.contains(fighter)]
+    if title and ('Yes' in list(df["TitleFight"])) | ('Interim' in list(df["TitleFight"])):
+        df = df[(df['TitleFight'] == 'Yes') | (df['TitleFight'] == 'Interim')]
 
     redDF = df.filter(regex='RED$', axis=1)
     redDF["Fight_Time_(Min)"] = df["Fight_Time_(sec)"] / 60
@@ -34,27 +39,35 @@ def opponentStats(fighter):
 
     return pd.concat([redDF, blueDF]).sort_index()
 
-def sideBySideStats(fighter):
-    f = pd.DataFrame(fighterStats(fighter).iloc[:, 1:].sum())
-    o = pd.DataFrame(opponentStats(fighter).iloc[:, 1:].sum())
+
+def sideBySideStats(fighter, title=False):
+    f = pd.DataFrame(fighterStats(fighter, title).iloc[:, 1:].sum())
+    o = pd.DataFrame(opponentStats(fighter, title).iloc[:, 1:].sum())
     combo = pd.merge(f, o, on=f.index)
     combo.rename(columns={'key_0': 'Fighter', '0_x': f'{fighter}', '0_y': 'Opponents'}, inplace=True)
     combo.set_index("Fighter", inplace=True)
     combo = combo.transpose()
     return combo
 
+
 if __name__ == "__main__":
-    # print(merged_df.iloc[:, 1:].sum())
-    f = pd.DataFrame(fighterStats("Conor McGregor").iloc[:, 1:].sum())
-
-    # print(merged_df.iloc[:, 1:].sum() / merged_df["Fight_Time_(Min)"].sum())
-    o = pd.DataFrame(opponentStats("Conor McGregor").iloc[:, 1:].sum())
-
-    combo = pd.merge(f, o, on=f.index)
-    combo.rename(columns={'key_0': 'Fighter', '0_x': f'{"Conor McGregor"}', '0_y': 'Opponents'}, inplace=True)
-    combo.set_index("Fighter", inplace=True)
-    combo = combo.transpose()
-    # print(combo)
-    print(sideBySideStats('Conor McGregor'))
-    # fig = px.bar(combo, combo.index, "KD")
-    # fig.show()
+    # df = pd.read_csv("C:\\Users\\sabzu\\Documents\\UFCRecommendationProject\\UFCProject\\DataFiles2\\CleanData.csv",
+    #                  index_col=0)
+    # title = False
+    # df = df[df["BOUT"].str.contains('Conor McGregor')]
+    # if title and ('Yes' in list(df["TitleFight"])) | ('Interim' in list(df["TitleFight"])):
+    #     df = df[(df['TitleFight'] == 'Yes') | (df['TitleFight'] == 'Interim')]
+    #
+    # redDF = df.filter(regex='RED$', axis=1)
+    # redDF["Fight_Time_(Min)"] = df["Fight_Time_(sec)"] / 60
+    # blueDF = df.filter(regex='BLUE$', axis=1)
+    # blueDF["Fight_Time_(Min)"] = df["Fight_Time_(sec)"] / 60
+    #
+    # redDF.columns = redDF.columns.str.replace('_RED', '')
+    # blueDF.columns = blueDF.columns.str.replace('_BLUE', '')
+    #
+    # redDF = (redDF[redDF["FIGHTER"] == 'Conor McGregor'])
+    # blueDF = (blueDF[blueDF["FIGHTER"] == 'Conor McGregor'])
+    #
+    # print(pd.concat([redDF, blueDF]).sort_index())
+    print(sideBySideStats('Sean OMalley', True))
