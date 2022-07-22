33f52309312685b00cbdef5c8b941fc02f8741b8
sabzuski <sabzuski@gmail.com>
2022-07-22 16:13:10 -0400

    made adjustment to allow for per minute calculation of stats in sidebysidestats

--- Scripts/fighterStats.py
+++ Scripts/fighterStats.py
@@ -42,17 +42,20 @@ def opponentStats(fighter, title=False):
 
 
 def sideBySideStats(fighter, metric, title=False):
-    if metric == "sum":
+    if (metric == "sum") | (metric != 'mean'):
         f = pd.DataFrame(fighterStats(fighter, title).iloc[:, 1:].sum())
         o = pd.DataFrame(opponentStats(fighter, title).iloc[:, 1:].sum())
-    else:
+    elif metric == "mean":
         f = pd.DataFrame(fighterStats(fighter, title).iloc[:, 1:].mean().round(2))
         o = pd.DataFrame(opponentStats(fighter, title).iloc[:, 1:].mean().round(2))
+
     combo = pd.merge(f, o, on=f.index)
     combo.rename(columns={'key_0': 'Fighter', '0_x': f'{fighter}', '0_y': 'Opponents'}, inplace=True)
     combo.set_index("Fighter", inplace=True)
     combo = combo.transpose()
-    return combo
+    if (metric != 'sum') & (metric != 'mean'):
+        combo.iloc[:, :-1] = combo.iloc[:, :-1].applymap(lambda x: x / combo.iloc[0,-1])
+    return combo.round(2)
 
 
 def individualFightStats(df):
@@ -77,42 +80,6 @@ if __name__ == "__main__":
     df = pd.read_csv("C:\\Users\\sabzu\\Documents\\UFCRecommendationProject\\UFCProject\\DataFiles2\\CleanData.csv",
                      index_col=0)
 
-    # ufc = individualFightStats(df)
-    #
-    # fightCount = ufc["WeightClass"].value_counts()
-    # ufcSumStatsGrouped = (ufc.groupby("WeightClass").mean())
-    # for i in ufcSumStatsGrouped.columns[:-1]:
-    #     ufcSumStatsGrouped[i] = ((ufcSumStatsGrouped[i] / ufcSumStatsGrouped["Fight_Time_(Min)"])).__round__(2)
-    #
-    # attributes = "CONTROL_TIME"
-    #
-    # attDict = {"Total Strikes": ["TOTAL_STR_ATT", "TOTAL_STR_LAND"], "Head Strikes": ["HEAD_ATT", "HEAD_LAND"],
-    #            "Body Strikes": ["BODY_ATT", "BODY_LAND"], "Leg Strikes": ["LEG_ATT", "LEG_LAND"],
-    #            "Standing Strikes": ["STD_STR_ATT", "STD_STR_LAND"],
-    #            "Clinch Strikes": ["CLINCH_STR_ATT", "CLINCH_STR_LAND"],
-    #            "Ground Strikes": ['GRD_STR_ATT', 'GRD_STR_LAND'], "Takedowns": ["TD_ATT", "TD"]}
-    #
-    # attribute1 = attributes
-    # ufcSumStatsGrouped = pd.DataFrame(ufcSumStatsGrouped)
-    #
-    # weightclass = ["Flyweight Bout", "Bantamweight Bout", "Featherweight Bout", "Lightweight Bout", "Welterweight Bout", "Middleweight Bout",
-    #                             "Light Heavyweight Bout", "Heavyweight Bout", "Catch Weight Bout", "Women's Strawweight Bout", "Women's Flyweight Bout",
-    #                             "Women's Bantamweight Bout", "Women's Featherweight Bout"]
-    #
-    #
-    # ufcSumStatsGrouped.insert(0, "WeightClass", ufcSumStatsGrouped.index)
-    # ufcSumStatsGrouped["WeightClass"] = pd.Categorical(ufcSumStatsGrouped['WeightClass'], weightclass)
-    #
-    # newind = list(range(len(ufcSumStatsGrouped)))
-    # ufcSumStatsGrouped.insert(0, "index", newind)
-    # ufcSumStatsGrouped.set_index('index', inplace=True)
-    # ufcSumStatsGrouped.sort_values("WeightClass", inplace=True)
-    # ufcSumStatsGrouped.reset_index(drop=True, inplace=True)
-    # print(ufcSumStatsGrouped)
-    # # ave1 = (np.mean(ufcSumStatsGrouped[attribute1]))
-    # # ave2 = (np.mean(ufcSumStatsGrouped[attribute2]))
-    #
-    # fig = px.line(ufcSumStatsGrouped, "WeightClass", f"{attribute1}")
-    # fig.show()
+    sideBySideStats("Conor McGregor", "noone")
 
 
